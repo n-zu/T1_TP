c7076c5b993c3c1a05e887fd200f603ac1458e88
efoppiano <efoppiano@fi.uba.ar>
2021-10-28 18:19:07 -0300

    Empiezo server con 1 thread por packet

--- server/src/server/mod.rs
+++ server/src/server/mod.rs
@@ -1,9 +1,9 @@
-use std::{collections::HashMap, io::{self, Read, Write}, net::{TcpListener, TcpStream}, sync::{
+use std::{collections::HashMap, io::{self, Read, Write}, net::{SocketAddr, TcpListener, TcpStream}, sync::{
         mpsc::{sync_channel, Receiver, Sender, SyncSender},
         Arc, Mutex,
-    }, thread, time::Duration};
+    }, thread::{self, JoinHandle}, time::Duration, vec};
 
-use crate::{connack::Connack, connect::Connect};
+use crate::{connack::Connack, connect::{self, Connect}};
 
 const MPSC_BUF_SIZE: usize = 256;
 const SLEEP_DUR: Duration = Duration::from_secs(2);
@@ -30,78 +30,123 @@ enum Packet {
 }
 
 struct Client {
-    stream: TcpStream,
-    pending: Option<Receiver<Packet>>,
+    incoming: TcpStream,
     id: Option<String>,
 }
 
 struct ClientData {
-    sender: Mutex<SyncSender<Packet>>
+    outgoing: Mutex<TcpStream>
 }
 
-struct Queue {
-    head: Mutex<Receiver<Client>>,
-    tail: SyncSender<Client>,
-}
-
-struct Topic {}
-
-struct Petition {
-    client: Client,
-    packet: Packet
-}
-
-struct Petitions {
-    sender: SyncSender<Petition>,
-    receiver: Mutex<Receiver<Petition>>,
-}
 struct Server {
-    queue: Queue,
-    petitions: Petitions,
-    client_datas: HashMap<String, ClientData>,
+    client_datas: Mutex<HashMap<String, ClientData>>,
     port: u16,
+    handlers: Mutex<Vec<JoinHandle<()>>>
 }
 
 impl Read for Client {
     fn read(&mut self, buf: &mut [u8]) -> std::io::Result<usize> {
-        self.stream.read(buf)
+        self.incoming.read(buf)
     }
 }
 
-impl Petitions {
-    fn new() -> Self {
-        let (sync_sender, receiver) = sync_channel(MPSC_BUF_SIZE);
+impl ClientData {
+    fn new(outgoing: TcpStream) -> Self {
         Self {
-            sender: sync_sender,
-            receiver: Mutex::new(receiver),
+            outgoing: Mutex::new(outgoing)
         }
     }
 }
 
-impl Queue {
-    fn new() -> Self {
-        let (sync_sender, receiver) = sync_channel(MPSC_BUF_SIZE);
-        Self {
-            head: Mutex::new(receiver),
-            tail: sync_sender,
+impl Server {
+    fn new(port: u16) -> Arc<Self> {
+        Arc::new(Self {
+            client_datas: Mutex::new(HashMap::new()),
+            port,
+            handlers: Mutex::new(vec![])
+        })
+    }
+
+    fn read_packet(&self, headers: [u8; 2], stream: &mut TcpStream) -> Packet {
+        let codigo = headers[0] >> 4;
+
+        match codigo {
+            1 => match connect::Connect::new(headers, stream) {
+                Ok(packet) => {
+                    Packet::ConnectType(packet)
+                }
+                Err(_) => {
+                    todo!();
+                }
+            },
+            2 => todo!("Pendiente implementación"),
+            3 => todo!("Pendiente implementación"),
+            4 => todo!("Pendiente implementación"),
+            5 => todo!("Pendiente implementación"),
+            6 => todo!("Pendiente implementación"),
+            7 => todo!("Pendiente implementación"),
+            8 => todo!("Pendiente implementación"),
+            9 => todo!("Pendiente implementación"),
+            10 => todo!("Pendiente implementación"),
+            11 => todo!("Pendiente implementación"),
+            12 => todo!("Pendiente implementación"),
+            13 => todo!("Pendiente implementación"),
+            14 => todo!("Pendiente implementación"),
+            _ => todo!("Error"),
         }
     }
 
-    fn enqueue(&self, client: Client) {
-        self.tail.send(client).unwrap();
-    }
+    fn receive_packet(&self, stream: &mut TcpStream) -> Packet {
+        let mut buf = [0u8, 2];
+        match stream.read_exact(&mut buf) {
+            Ok(_size) => {
+                self.read_packet(buf, stream)
+            }
+            Err(_) => todo!()
+            /*
+            Err(error) if error.kind() == io::ErrorKind::UnexpectedEof => {
+                println!("Cliente se desconecto sin avisar");
+            }
 
-    fn dequeue(&self) -> Option<Client> {
-        if let Ok(client) = self.head.lock().unwrap().try_recv() {
-            return Some(client);
+            Err(err) => {
+                println!(
+                    "Error recibiendo bytes de stream: {}\n Kind: {:?}",
+                    err.to_string(),
+                    err.kind()
+                );
+            }
+            */
         }
-        None
     }
-}
 
-fn listen_for_clients(queue_tail: SyncSender<Client>) {
-    let listener = TcpListener::bind(format!("127.0.0.1:{}", 1883)).unwrap();
-    loop {
+    fn handle_packet(&self, packet: Packet) {
+
+    }
+    
+    fn manage_client(self: Arc<Self>, mut stream: TcpStream, addr: SocketAddr) {
+        loop {
+            let packet = self.receive_packet(&mut stream);
+            let sv_copy = self.clone();
+            let handle = thread::spawn(move || {
+                sv_copy.handle_packet(packet);
+            });
+        }
+    }
+
+    fn connect(self: &Arc<Self>, stream: TcpStream, addr: SocketAddr) {
+        println!("Conectando con {}", addr.to_string());
+        if let Err(_err) = stream.set_nonblocking(true) {
+            println!("Error estableciendo socket como no bloqueante");
+        } else {
+            let sv_copy = self.clone();
+            let handle = thread::spawn(move || {
+                sv_copy.manage_client(stream, addr);
+            });
+            self.handlers.lock().unwrap().push(handle);
+        }
+    }
+
+    fn accept_client(self: &Arc<Self>, listener: &TcpListener) {
         match listener.accept() {
             Err(error) if error.kind() == io::ErrorKind::WouldBlock => {
                 thread::sleep(SLEEP_DUR);
@@ -110,125 +155,30 @@ fn listen_for_clients(queue_tail: SyncSender<Client>) {
                 println!("Error aceptando conexión: {}", error.to_string());
             }
             Ok((stream, addr)) => {
-                println!("Conectando con {}", addr.to_string());
-                if let Err(_err) = stream.set_nonblocking(true) {
-                    println!("Error estableciendo socket como no bloqueante");
-                } else if let Err(_err) = queue_tail.send(Client::new(stream)) {
-                    println!("Error enviando cliente");
-                }
+                self.connect(stream, addr);
             }
         }
     }
-}
 
-impl ClientData {
-    fn new(sender: SyncSender<Packet>) -> Self {
-        Self {
-            sender: Mutex::new(sender)
-        }
-    }
-}
-
-fn handle_petitions(server: &mut Server) {
-    while let Ok(mut petition) = server.petitions.receiver.lock().unwrap().try_recv() {
-        println!("Manejando petition particular");
-        match petition.packet {
-            Packet::ConnectType(connect) => {
-                println!("Es un connect");
-                let (tx, rx) = sync_channel(MPSC_BUF_SIZE);
-                tx.send(Packet::ConnackType(connect.response().clone())).unwrap();
-                petition.client.set_pending(rx);
-                let _ = server.client_datas
-                .insert(petition.client.id.as_ref().unwrap().to_owned(), ClientData::new(tx)).is_none();
-                server.queue.enqueue(petition.client);
-
-            }
-            _ => todo!()
-        }
-    }
-}
-
-impl Server {
-    fn new(port: u16) -> Arc<Self> {
-        Arc::new(Self {
-            queue: Queue::new(),
-            petitions: Petitions::new(),
-            client_datas: HashMap::new(),
-            port,
-        })
-    }
-    fn next_client(&self) -> Option<Client> {
-        self.queue.dequeue()
-    }
-
-    fn dispatched(&self, client: Client) {
-        self.queue.enqueue(client);
-    }
-
-    fn run(mut self: Arc<Self>) {
-        let tail = self.queue.tail.clone();
-        let _ = thread::spawn(move || {
-            listen_for_clients(tail)
-        });
-
+    fn run(self: Arc<Self>) {
+        let listener = TcpListener::bind(format!("127.0.0.1:{}", 1883)).unwrap();
         loop {
-            let copy = self.clone();
-
-            let handle = thread::spawn(move || dispatch(&copy));
-            dispatch(&self);
-            handle.join().unwrap();
-            println!("Manejando petitions");
-            handle_petitions(Arc::get_mut(&mut self).unwrap());
-            thread::sleep(SLEEP_DUR)
+            self.accept_client(&listener); 
         }
-    }
-
-    fn new_client(&mut self, client: Client, sync_sender: SyncSender<Packet>) {
-        self.queue.enqueue(client);
-    }
-
-    fn new_petition(&self, client: Client, packet: Packet) {
-        self.petitions.sender.send(Petition { client: client, packet }).unwrap();
     }
 }
 
 impl Client {
-    fn new(stream: TcpStream) -> Client {
+    fn new(incoming: TcpStream) -> Client {
         Self {
             id: None,
-            stream: stream,
-            pending: None,
+            incoming,
         }
-    }
-
-    fn set_pending(&mut self, pending: Receiver<Packet>) {
-        if self.pending.is_some() {
-            panic!("Cliente ya tiene pending");
-        }
-        self.pending = Some(pending);
-    }
-
-    fn pending(&self) -> Option<Packet> {
-        if let Ok(pending) = self.pending.as_ref().unwrap().try_recv() {
-            return Some(pending);
-        }
-        return None;
     }
 
     fn new_id(&mut self, id: &str) {
         self.id = Some(id.to_owned())
     }
-
-    //fn send(&mut self, packet: Packet) {
-    //    self.stream.write(&packet.as_bytes()).unwrap();
-    //}
-    /*
-    fn receive_packet(&mut self) -> Option<impl Packet> {
-        let mut buff: [u8; 1000] = [0; 1000];
-        self.stream.read(&mut buff).unwrap();
-        return Some(PacketMock::new_mock());
-    }
-    */
 }
 
 fn packet_maker(headers: [u8; 2], client: &mut Client) -> Option<Packet> {
@@ -282,38 +232,6 @@ fn get_packet(client: &mut Client) -> Option<Packet> {
             );
             None
         }
-    }
-}
-
-fn dispatch(server: &Server) {
-    if let Some(mut client) = server.next_client() {
-        if let Some(packet) = get_packet(&mut client) {
-            println!("habia packet");
-            match packet {
-                Packet::ConnectType(connect) => {
-                    println!("Recibido un Connect, client_id: {}", connect.client_id());
-                    client.new_id(connect.client_id());
-                    let id = connect.client_id().to_owned();
-                    server.new_petition(client, Packet::ConnectType(connect));
-                    return;
-                }
-                _ => todo!()
-            }
-        }
-        println!("llego");
-        if let Some(packet) = client.pending() {
-            match packet {
-                Packet::ConnectType(_connect) => {
-                    // No deberia pasar
-                    todo!();
-                }
-                Packet::ConnackType(connack) => {
-                    println!("Mandando un CONNACK");
-                    client.stream.write_all(&connack.encode()).unwrap();
-                }
-            }
-        }
-        println!("despachado");
     }
 }
 
