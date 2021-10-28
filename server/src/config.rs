8233dbc23d58b4673e47acc6fc445427276e090b
efoppiano <efoppiano@fi.uba.ar>
2021-10-27 22:49:00 -0300

    Pule detalles y agrega tests

--- server/src/config.rs
+++ server/src/config.rs
@@ -3,25 +3,59 @@ use std::{
     io::{BufRead, BufReader, Read},
 };
 
+/// Contiene toda la informacion de configuracion
+/// necesaria para iniciar el servidor
 pub struct Config {
+    /// Puerto al que se conecta el servidor
     port: u16,
+    /// Path del archivo sobre el cual se realizara un dump
     dump_path: String,
+    /// Intervalo de tiempo para el cual se realizara un dump
     dump_time: u32,
+    /// Path de archivo de log
     log_path: String,
 }
 
 impl Config {
+    /// Crea un struct Config a partir de la informacion
+    /// que se encuentran en el archivo de configuracion
+    /// 
+    /// # Arguments
+    /// 
+    /// * `path` - Ruta del archivo que contiene la informacion.
+    /// El formato del archivo es 'campo=valor' (sin espacios)
+    /// en el siguiente orden: port, dump_path, dump_time, log_path
+    /// 
+    /// # Returns
+    /// 
+    /// * `Option<Config>` - Struct con la configuracion del servidor.
+    /// 
+    /// # Errors
+    /// Si no existe el archivo o el formato es invalido, devuelve None
     pub fn new(path: &str) -> Option<Config> {
         let config_file = File::open(path).ok()?;
+        Config::new_from_file(config_file)
+    }
+
+    /// Crea un struct Config a partir de un archivo de configuracion
+    /// 
+    /// # Arguments
+    /// 
+    /// * `config_file` - Archivo de configuracion
+    /// 
+    /// # Return
+    /// 
+    /// * `Option<Config>` - Struct con la configuracion del servidor
+    pub fn new_from_file<F: Read>(config_file: F) -> Option<Config> {
         let mut buffered = BufReader::new(config_file);
 
-        let port = get_value_from_line(&mut buffered, "port");
-        let dump_path = get_value_from_line(&mut buffered, "dump_path");
-        let dump_time = get_value_from_line(&mut buffered, "dump_time");
-        let log_path = get_value_from_line(&mut buffered, "log_path");
+        let port = get_value_from_line(&mut buffered, "port")?;
+        let dump_path = get_value_from_line(&mut buffered, "dump_path")?;
+        let dump_time = get_value_from_line(&mut buffered, "dump_time")?;
+        let log_path = get_value_from_line(&mut buffered, "log_path")?;
 
-        let port = port.parse::<u16>().unwrap();
-        let dump_time = dump_time.parse::<u32>().unwrap();
+        let port = port.parse::<u16>().ok()?;
+        let dump_time = dump_time.parse::<u32>().ok()?;
 
         Some(Config {
             port,
@@ -31,34 +65,46 @@ impl Config {
         })
     }
 
+    /// Devuelve el puerto del servidor
     pub fn port(&self) -> u16 {
         self.port
     }
+    
+    /// Devuelve el path del archivo sobre el cual se realizara un dump
+    pub fn dump_path(&self) -> &str {
+        &self.dump_path
+    }
 
+    /// Devuelve el intervalo de tiempo para el cual se realizara un dump
     pub fn dump_time(&self) -> u32 {
         self.dump_time
     }
+
+    /// Devuelve el path de archivo de log
+    pub fn log_path(&self) -> &str {
+        &self.log_path
+    }
 }
 
-fn get_value_from_line<F>(buffered: &mut BufReader<F>, expected_key: &str) -> String
+fn get_value_from_line<F>(buffered: &mut BufReader<F>, expected_key: &str) -> Option<String>
 where
     F: Read,
 {
     let mut buf = String::new();
 
-    buffered.read_line(&mut buf).unwrap();
+    buffered.read_line(&mut buf).ok()?;
     let key_value = buf.trim().split('=').collect::<Vec<&str>>();
     if key_value.len() != 2 {
-        panic!("Error de split de linea de Config");
+        return None;
     }
     let key = key_value[0];
     let value = key_value[1];
     if !key.eq(expected_key) {
-        panic!("Se esperaba key {}, se encontro {}", expected_key, key);
+        return None;
     }
-    value.to_owned()
+    Some(value.to_owned())
 }
-/*
+
 #[cfg(test)]
 mod tests {
     use std::io::Cursor;
@@ -66,30 +112,50 @@ mod tests {
     use crate::config::Config;
 
     #[test]
-    fn ok_test() {
-        let mut cursor = Cursor::new(
+    fn valid_file() {
+        let cursor = Cursor::new(
 "port=8080
 dump_path=foo.txt
 dump_time=10
 log_path=bar.txt");
 
-        let config = Config::new(&mut cursor);
-        assert_eq!(config.port, 8080);
-        assert_eq!(config.dump_path, "foo.txt");
-        assert_eq!(config.dump_time, 10);
-        assert_eq!(config.log_path, "bar.txt");
+        let config = Config::new_from_file(cursor).unwrap();
+        assert_eq!(config.port(), 8080);
+        assert_eq!(config.dump_path(), "foo.txt");
+        assert_eq!(config.dump_time(), 10);
+        assert_eq!(config.log_path(), "bar.txt");
     }
 
     #[test]
-    #[should_panic]
-    fn bad_key() {
-        let mut cursor = Cursor::new(
+    fn invalid_key() {
+        let cursor = Cursor::new(
 "invalid_key=8080
 dump_path=foo.txt
 dump_time=10
 log_path=bar.txt");
 
-        Config::new(&mut cursor);
+        assert!(Config::new_from_file(cursor).is_none());
+    }
+
+    #[test]
+    fn invalid_order() {
+        let cursor = Cursor::new(
+"port=8080
+dump_time=10
+dump_path=foo.txt
+log_path=bar.txt");
+
+        assert!(Config::new_from_file(cursor).is_none());
+    }
+
+    #[test]
+    fn invalid_value() {
+        let cursor = Cursor::new(
+"port=WWWW
+dump_path=foo.txt
+dump_time=10
+log_path=bar.txt");
+            
+        assert!(Config::new_from_file(cursor).is_none());
     }
 }
-*/
