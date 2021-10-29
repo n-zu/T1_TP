0839118caaca809e6a7e86a11a31065e892b317f
efoppiano <efoppiano@fi.uba.ar>
2021-10-28 22:14:40 -0300

    Avanza conexion con el cliente

--- server/src/server/mod.rs
+++ server/src/server/mod.rs
@@ -1,33 +1,36 @@
-use std::{collections::HashMap, io::{self, Read, Write}, net::{SocketAddr, TcpListener, TcpStream}, sync::{
+#![allow(dead_code, unused_variables)]
+
+use core::panic;
+use std::{
+    collections::HashMap,
+    io::{self, Read, Write},
+    net::{SocketAddr, TcpListener, TcpStream},
+    sync::{
         mpsc::{sync_channel, Receiver, Sender, SyncSender},
         Arc, Mutex,
-    }, thread::{self, JoinHandle}, time::Duration, vec};
+    },
+    thread::{self, JoinHandle},
+    time::Duration,
+    vec,
+};
 
-use crate::{connack::Connack, connect::{self, Connect}};
+use packets::packet_reader::{ErrorKind, PacketError};
 
+use crate::{
+    connack::Connack,
+    connect::{self, Connect},
+};
 const MPSC_BUF_SIZE: usize = 256;
 const SLEEP_DUR: Duration = Duration::from_secs(2);
-
-/*
-trait Packet {
-    fn new<T: Read>(stream: &mut T) -> Result<Box<Self>, String>;
-
-    // Nota: Debe recibir como parametro al servidor/cliente
-    fn process(&self, server: &Server);
-
-    // Devuelve el paquete que debe enviarse como respuesta
-    fn reply(&self, server: &Server) -> Option;
-
-    fn as_bytes(self) -> Vec<u8>;
-
-    //fn packet_type(&self) -> PacketType;
-}
-*/
 
 enum Packet {
     ConnectType(Connect),
     ConnackType(Connack),
+    PublishTypee(Publish),
 }
+
+// Temporal
+struct Publish {}
 
 struct Client {
     incoming: TcpStream,
@@ -35,13 +38,13 @@ struct Client {
 }
 
 struct ClientData {
-    outgoing: Mutex<TcpStream>
+    outgoing: Mutex<TcpStream>,
 }
 
 struct Server {
     client_datas: Mutex<HashMap<String, ClientData>>,
     port: u16,
-    handlers: Mutex<Vec<JoinHandle<()>>>
+    handlers: Mutex<Vec<JoinHandle<()>>>,
 }
 
 impl Read for Client {
@@ -53,7 +56,7 @@ impl Read for Client {
 impl ClientData {
     fn new(outgoing: TcpStream) -> Self {
         Self {
-            outgoing: Mutex::new(outgoing)
+            outgoing: Mutex::new(outgoing),
         }
     }
 }
@@ -63,20 +66,17 @@ impl Server {
         Arc::new(Self {
             client_datas: Mutex::new(HashMap::new()),
             port,
-            handlers: Mutex::new(vec![])
+            handlers: Mutex::new(vec![]),
         })
     }
 
     fn read_packet(&self, headers: [u8; 2], stream: &mut TcpStream) -> Packet {
         let codigo = headers[0] >> 4;
-
         match codigo {
             1 => match connect::Connect::new(headers, stream) {
-                Ok(packet) => {
-                    Packet::ConnectType(packet)
-                }
-                Err(_) => {
-                    todo!();
+                Ok(packet) => Packet::ConnectType(packet),
+                Err(err) => {
+                    todo!("{:?} {:?}", err.kind(), err.to_string());
                 }
             },
             2 => todo!("Pendiente implementación"),
@@ -96,40 +96,64 @@ impl Server {
         }
     }
 
-    fn receive_packet(&self, stream: &mut TcpStream) -> Packet {
+    fn receive_packet(&self, stream: &mut TcpStream) -> Result<Packet, PacketError> {
         let mut buf = [0u8, 2];
         match stream.read_exact(&mut buf) {
-            Ok(_size) => {
-                self.read_packet(buf, stream)
-            }
-            Err(_) => todo!()
-            /*
+            Ok(_size) => Ok(self.read_packet(buf, stream)),
             Err(error) if error.kind() == io::ErrorKind::UnexpectedEof => {
                 println!("Cliente se desconecto sin avisar");
+                Err(PacketError::new_kind(
+                    "Cliente se desconecto sin avisar",
+                    ErrorKind::ClientDisconnected,
+                ))
             }
-
+            Err(error) if error.kind() == io::ErrorKind::WouldBlock => Err(PacketError::new()),
             Err(err) => {
-                println!(
+                panic!(
                     "Error recibiendo bytes de stream: {}\n Kind: {:?}",
                     err.to_string(),
                     err.kind()
                 );
             }
-            */
         }
     }
 
-    fn handle_packet(&self, packet: Packet) {
-
+    fn handle_connect(&self, packet: Connect, mut stream: TcpStream) {
+        let id = packet.client_id();
+        stream
+            .write_all(&packet.response().to_owned().encode())
+            .unwrap();
     }
-    
-    fn manage_client(self: Arc<Self>, mut stream: TcpStream, addr: SocketAddr) {
+
+    fn handle_packet(&self, packet: Packet, stream: TcpStream) {
+        match packet {
+            Packet::ConnectType(packet) => self.handle_connect(packet, stream),
+            Packet::ConnackType(_) => todo!(),
+            Packet::PublishTypee(packet) => todo!(),
+        }
+    }
+
+    fn manage_client(self: Arc<Self>, mut stream: TcpStream, _addr: SocketAddr) {
+        let mut handlers = vec![];
         loop {
-            let packet = self.receive_packet(&mut stream);
-            let sv_copy = self.clone();
-            let handle = thread::spawn(move || {
-                sv_copy.handle_packet(packet);
-            });
+            match self.receive_packet(&mut stream) {
+                Ok(packet) => {
+                    let sv_copy = self.clone();
+                    let stream_copy = stream.try_clone().unwrap();
+                    let handle = thread::spawn(move || {
+                        sv_copy.handle_packet(packet, stream_copy);
+                    });
+                    handlers.push(handle);
+                }
+                Err(err) if err.kind() == ErrorKind::ClientDisconnected => break,
+                Err(_) => {
+                    //println!("No se recibio ningun paquete");
+                    //thread::sleep(SLEEP_DUR);
+                }
+            }
+        }
+        for handle in handlers {
+            handle.join().unwrap();
         }
     }
 
@@ -160,20 +184,21 @@ impl Server {
         }
     }
 
+    pub fn send_publish_to(&self, packet: Publish, client_id: &str) {
+        todo!()
+    }
+
     fn run(self: Arc<Self>) {
         let listener = TcpListener::bind(format!("127.0.0.1:{}", 1883)).unwrap();
         loop {
-            self.accept_client(&listener); 
+            self.accept_client(&listener);
         }
     }
 }
 
 impl Client {
     fn new(incoming: TcpStream) -> Client {
-        Self {
-            id: None,
-            incoming,
-        }
+        Self { id: None, incoming }
     }
 
     fn new_id(&mut self, id: &str) {
@@ -185,9 +210,7 @@ fn packet_maker(headers: [u8; 2], client: &mut Client) -> Option<Packet> {
     let codigo = headers[0] >> 4;
     match codigo {
         1 => match Connect::new(headers, client) {
-            Ok(packet) => {
-                Some(Packet::ConnectType(packet))
-            }
+            Ok(packet) => Some(Packet::ConnectType(packet)),
             Err(err) => {
                 println!("Error parseando Connect packet: {}", err.to_string());
                 None
@@ -210,31 +233,6 @@ fn packet_maker(headers: [u8; 2], client: &mut Client) -> Option<Packet> {
     }
 }
 
-fn get_packet(client: &mut Client) -> Option<Packet> {
-    let mut buf = [0u8, 2];
-    match client.read_exact(&mut buf) {
-        Ok(_size) => {
-            packet_maker(buf, client)
-        }
-        Err(error) if error.kind() == io::ErrorKind::WouldBlock => {
-            None
-        }
-        Err(error) if error.kind() == io::ErrorKind::UnexpectedEof => {
-            println!("Cliente se desconecto sin avisar");
-            None
-        }
-
-        Err(err) => {
-            println!(
-                "Error recibiendo bytes de stream: {}\n Kind: {:?}",
-                err.to_string(),
-                err.kind()
-            );
-            None
-        }
-    }
-}
-
 #[cfg(test)]
 mod tests {
     use std::{
@@ -249,45 +247,11 @@ mod tests {
 
     use super::{Packet, Server};
 
+    /*
     #[test]
     fn test() {
         let server = Server::new(1883);
         server.run()
-    }
-    /*
-    #[test]
-    fn test() {
-        let handle = thread::spawn(|| {
-            cliente();
-        });
-
-        let mut client = None;
-        let (sync_sender, receiver) = sync_channel(MPSC_BUF_SIZE);
-        let listener = TcpListener::bind(format!("127.0.0.1:{}", 1234)).unwrap();
-        match listener.accept() {
-            Err(error) if error.kind() == io::ErrorKind::WouldBlock => {
-                thread::sleep(Duration::from_millis(5));
-            }
-            Err(_error) => {
-                //println!("Error aceptando conexión: {}", error.to_string());
-            }
-            Ok((stream, _addr)) => {
-                client = Some(Client::new("rust", stream, receiver));
-            }
-        }
-
-        let mut server = Server::new(1234);
-        {
-            let server = Arc::get_mut(&mut server).unwrap();
-            server.new_client(client.unwrap(), sync_sender);
-        }
-        server.run();
-        handle.join().unwrap();
-    }
-
-    fn cliente() {
-        let _stream = TcpStream::connect(format!("127.0.0.1:{}", 1234)).unwrap();
-        thread::sleep(Duration::from_millis(3));
     }
     */
 }
