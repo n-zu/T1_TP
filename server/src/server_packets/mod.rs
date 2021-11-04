ed0b57d7440279e2d0c4c8a834dfa661ca0bbf65
efoppiano <efoppiano@fi.uba.ar>
2021-11-04 13:06:12 -0300

    Implementa parseo PingReq

--- server/src/server_packets/mod.rs
+++ server/src/server_packets/mod.rs
@@ -1,7 +1,9 @@
 pub mod connack;
 pub mod connect;
+pub mod pingreq;
 pub mod subscribe;
 
 pub use connack::Connack;
 pub use connect::Connect;
+pub use pingreq::PingReq;
 pub use subscribe::Subscribe;
