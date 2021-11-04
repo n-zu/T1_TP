f9b8b7978386daeb8dabc23196183f4fdb7d9011
efoppiano <efoppiano@fi.uba.ar>
2021-11-04 13:28:00 -0300

    Format

--- server/src/server_packets/mod.rs
+++ server/src/server_packets/mod.rs
@@ -1,11 +1,11 @@
 pub mod connack;
 pub mod connect;
-pub mod pingreq;
 pub mod disconnect;
+pub mod pingreq;
 pub mod subscribe;
 
 pub use connack::Connack;
 pub use connect::Connect;
-pub use pingreq::PingReq;
 pub use disconnect::Disconnect;
+pub use pingreq::PingReq;
 pub use subscribe::Subscribe;
