bff7647e5102227cf5125fd91b5f75cfeb1ffc03
Facundo Mastricchio <facundomastricchio@gmail.com>
2021-11-06 16:52:39 -0300

    Agrego test creacion unsuback

--- server/src/server_packets/unsuback.rs
+++ server/src/server_packets/unsuback.rs
@@ -1,3 +1,4 @@
+#![allow(unused)]
 use packets::packet_reader::{ErrorKind, PacketError};
 
 #[doc(hidden)]
@@ -5,16 +6,33 @@ const MSG_INVALID_PACKET_ID: &str = "Packet identifier must be greater than zero
 const CONTROL_BYTE: u8 = 0b10110000;
 const FIXED_REMAINING_LENGTH: u8 = 0b10;
 
+#[derive(Debug, PartialEq)]
+/// The UNSUBACK Packet is sent by the Server to the Client to confirm receipt of an UNSUBSCRIBE Packet.
 pub struct Unsuback {
     packet_id: u16,
 }
 
 impl Unsuback {
+    /// Returns a new Unsuback packet with given packet_id
+    ///
+    /// # Errors
+    ///
+    /// If packet_id is zero, this functions returns a [ErrorKind::InvalidProtocol]
     pub fn new(packet_id: u16) -> Result<Unsuback, PacketError> {
         Self::verify_packet_id(&packet_id)?;
         Ok(Self { packet_id })
     }
 
+    /// Encodes a Unsuback packet into its bytes representation following MQTT v3.1.1 protocol
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// let unsuback = Unsuback::new(1).unwrap();
+    /// let result = unsuback.encode();
+    /// let expected: Vec<u8> = vec![CONTROL_BYTE, FIXED_REMAINING_LENGTH, 0, 1];
+    /// assert_eq!(expected, result)
+    /// ```
     pub fn encode(&self) -> Vec<u8> {
         let mut bytes: Vec<u8> = vec![];
         bytes.append(&mut self.fixed_header());
@@ -40,7 +58,6 @@ impl Unsuback {
     }
 
     #[doc(hidden)]
-    /// The variable header contains the Packet Identifier of the UNSUBSCRIBE Packet that is being acknowledged
     fn variable_header(&self) -> Vec<u8> {
         let mut variable_header_buffer: Vec<u8> = vec![];
         let packet_id_representation = self.packet_id.to_be_bytes();
@@ -51,4 +68,25 @@ impl Unsuback {
 }
 
 #[cfg(test)]
-mod tests {}
+mod tests {
+    use crate::server_packets::unsuback::{
+        Unsuback, CONTROL_BYTE, FIXED_REMAINING_LENGTH, MSG_INVALID_PACKET_ID,
+    };
+    use packets::packet_reader::{ErrorKind, PacketError};
+
+    #[test]
+    fn test_unsuback_with_packet_id_0_should_raise_invalid_protocol_error() {
+        let result = Unsuback::new(0).unwrap_err();
+        let expected_error =
+            PacketError::new_kind(MSG_INVALID_PACKET_ID, ErrorKind::InvalidProtocol);
+        assert_eq!(expected_error, result)
+    }
+
+    #[test]
+    fn test_encoding_unsuback_packet_with_packet_id_1() {
+        let unsuback = Unsuback::new(1).unwrap();
+        let result = unsuback.encode();
+        let expected: Vec<u8> = vec![CONTROL_BYTE, FIXED_REMAINING_LENGTH, 0, 1];
+        assert_eq!(expected, result)
+    }
+}
