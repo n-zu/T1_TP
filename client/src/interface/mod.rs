bfd547655625dae227ec5241b3e31d9969ed8a89
Facundo Mastricchio <facundomastricchio@gmail.com>
2021-11-12 01:16:06 -0300

    Inicio de parseo de last will

--- client/src/interface/mod.rs
+++ client/src/interface/mod.rs
@@ -89,17 +89,26 @@ impl Controller {
         let password_entry: Entry = self.builder.object("con_psw").unwrap();
         let keep_alive_entry: Entry = self.builder.object("con_ka").unwrap();
         let clean_session_switch: Switch = self.builder.object("con_cs").unwrap();
+        let last_will_retain_switch: Switch = self.builder.object("con_lw_ret").unwrap();
+        let last_will_topic_entry: Entry = self.builder.object("con_lw_top").unwrap();
+        let last_will_msg_entry: Entry = self.builder.object("con_lw_msg").unwrap();
+        let last_will_qos_entry: Entry = self.builder.object("entry_qos_combobox").unwrap();
 
         let full_addr = format!(
             "{}:{}",
             &address_entry.text().to_string(),
             &port_entry.text().to_string()
         );
+
         let user_name = user_entry.text().to_string();
         let password = password_entry.text().to_string();
-        let keep_alive = keep_alive_entry.text().to_string().parse::<u16>().unwrap();
+        let keep_alive = keep_alive_entry.text().to_string().parse::<u16>()?;
         let client_id = id_entry.text().to_string();
         let clean_session = clean_session_switch.is_active();
+        let _last_will_retain = last_will_retain_switch.is_active();
+        let _last_will_topic = last_will_topic_entry.text().to_string();
+        let _last_will_msg = last_will_msg_entry.text().to_string();
+        let _last_will_qos = last_will_qos_entry.text().to_string().parse::<u8>()?;
 
         let connect = Self::_create_connect_packet(
             &client_id,
