ef56d02e651aa9e2f9f354d90f00a191f0a5ec5a
nicolaszulaica <nzulaica@fi.uba.ar>
2022-01-01 19:33:24 -0300

    refactor

--- thermometer/src/main.rs
+++ thermometer/src/main.rs
@@ -7,18 +7,21 @@ mod publisher_config;
 const MAX_TEMP: f32 = 100.0;
 const MIN_TEMP: f32 = 0.0;
 
-fn get_temperature() -> f32 {
-    thread_rng().gen::<f32>() * (MAX_TEMP - MIN_TEMP) + MIN_TEMP
-}
-
-fn main() {
+fn get_config() -> PublisherConfig {
     let args: Vec<String> = env::args().collect();
     let mut path: &str = "./config.txt";
     if args.len() > 1 {
         path = &args[1];
     }
-    let config = PublisherConfig::new(path).expect("Invalid config file");
+    PublisherConfig::new(path).expect("Invalid config file")
+}
 
+fn get_temperature() -> f32 {
+    thread_rng().gen::<f32>() * (MAX_TEMP - MIN_TEMP) + MIN_TEMP
+}
+
+fn main() {
+    let config = get_config();
     println!("{:?}", config);
 
     let y = get_temperature();
