1e381e312042afacbc8a22840985cd486a9417d1
fdelu <fdeluca@fi.uba.ar>
2022-02-06 16:19:40 -0300

    Agrego tests

--- http_final/common/config/src/config.rs
+++ http_final/common/config/src/config.rs
@@ -31,6 +31,7 @@ impl Config {
             .ok()?;
         let mut config: HashMap<String, String> = lines
             .iter()
+            .filter(|l| !l.is_empty())
             .map(|line| {
                 let (key, value) = line.trim().split_once(SEP)?;
                 Some((key.to_string(), value.to_string()))
@@ -46,5 +47,50 @@ impl Config {
             password: config.remove("password")?,
             period: Duration::from_millis(config.remove("period")?.parse().ok()?),
         })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::{io::Cursor, time::Duration};
+
+    use crate::config::{Config, SEP};
+
+    #[test]
+    fn test_new_from_file() {
+        let text = Cursor::new(
+            "
+            server=localhost
+            port=1883
+            client_id=test_client
+            topic=test_topic
+            user=test_user
+            password=test_password
+            period=1000"
+                .replace("=", SEP),
+        );
+        let config = Config::new_from_file(text).unwrap();
+        assert_eq!(config.server, "localhost");
+        assert_eq!(config.port, "1883");
+        assert_eq!(config.client_id, "test_client");
+        assert_eq!(config.topic, "test_topic");
+        assert_eq!(config.user, "test_user");
+        assert_eq!(config.password, "test_password");
+        assert_eq!(config.period, Duration::from_millis(1000));
+    }
+
+    #[test]
+    fn test_missing_fields() {
+        let text = Cursor::new(
+            "
+            server=localhost
+            client_id=test_client
+            topic=test_topic
+            user=test_user
+            period=1000"
+                .replace("=", SEP),
+        );
+        let config = Config::new_from_file(text);
+        assert!(config.is_none());
     }
 }
